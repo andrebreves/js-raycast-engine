510af3965bee58d136b88bb8703aec7c24184fa5
André Breves <andre.breves@gmail.com>
2017-08-29 00:17:50 -0300

    Segue controles do Wolfestein 3D

--- engine/Input.js
+++ engine/Input.js
@@ -1,8 +1,8 @@
 class Input {
 
   constructor() {
-    this.state = { left: false, right: false, forward: false, backward: false, strafeLeft: false, strafeRight: false };
-    this.keyMap = { 37: 'left', 39: 'right', 38: 'forward', 87: 'forward', 83: 'backward', 40: 'backward', 65: 'strafeLeft', 68: 'strafeRight' };
+    this.state = { left: false, right: false, forward: false, backward: false, strafe: false };
+    this.keyMap = { 37: 'left', 39: 'right', 38: 'forward', 40: 'backward', 18: 'strafe' };
     window.addEventListener('keydown', e => this.keyDown(e), false);
     window.addEventListener('keyup', e => this.keyUp(e), false);
   }
@@ -26,15 +26,15 @@ class Input {
   update(player) {
     const state = Object.assign({}, this.state);
 
-    if (state.left && !state.right) player.turnLeft();
-    if (state.right && !state.left) player.turnRight();
+    if (state.left  && !state.right && !state.strafe) player.turnLeft();
+    if (state.right && !state.left  && !state.strafe) player.turnRight();
+    if (state.left  && !state.right && state.strafe) player.strafeLeft();
+    if (state.right && !state.left  && state.strafe) player.strafeRight();
     if (state.forward && !state.backward) player.moveForward();
     if (state.backward && !state.forward) player.moveBackward();
-    if (state.strafeLeft && !state.strafeRight) player.strafeLeft();
-    if (state.strafeRight && !state.strafeLeft) player.strafeRight();
 
     if ((state.forward && !state.backward) || (state.backward && !state.forward) ||
-    (state.strafeLeft && !state.strafeRight) || (state.strafeRight && !state.strafeLeft)) {
+    (state.left  && !state.right && state.strafe) || (state.right && !state.left  && state.strafe)) {
       player.setWalking(true);
     } else {
       player.setWalking(false);
